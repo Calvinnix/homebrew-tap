c850b391ea1c6f6efc24e23f7e820a3d6dee44d0
Calvin Nix <Calvinnix@users.noreply.github.com>
2026-01-19 10:39:08 -0500

    Update simple-git to v0.5.0

--- Formula/simple-git.rb
+++ Formula/simple-git.rb
@@ -1,28 +1,28 @@
 class SimpleGit < Formula
   desc "Lightweight Git TUI"
   homepage "https://github.com/Calvinnix/simple-git"
-  version "0.4.0"
+  version "0.5.0"
   license "MIT"
 
   on_macos do
     on_arm do
-      url "https://github.com/Calvinnix/simple-git/releases/download/v0.4.0/simple-git-darwin-arm64.tar.gz"
-      sha256 "28f99ba8b0f7a7d3355b0ef77768badc6b9871d20141209202b00483edd29106"
+      url "https://github.com/Calvinnix/simple-git/releases/download/v0.5.0/simple-git-darwin-arm64.tar.gz"
+      sha256 "fb750c1c2a639878b9e14fae59308804a308e00d03b45b5f67139d69fb0ad739"
     end
     on_intel do
-      url "https://github.com/Calvinnix/simple-git/releases/download/v0.4.0/simple-git-darwin-amd64.tar.gz"
-      sha256 "2bedf039c391b2fca4e68bd33c11e99e8a64f03f0bd30fdb5fc4b255a88d17bc"
+      url "https://github.com/Calvinnix/simple-git/releases/download/v0.5.0/simple-git-darwin-amd64.tar.gz"
+      sha256 "27da092f2937380dfae2e91ba9eb3196d764c7e36ecc06680d116b960b75f9f2"
     end
   end
 
   on_linux do
     on_arm do
-      url "https://github.com/Calvinnix/simple-git/releases/download/v0.4.0/simple-git-linux-arm64.tar.gz"
-      sha256 "c8a138efe29739c79b6409f685c1f2ccca1b4348f20ffcd185f068e7e0083c94"
+      url "https://github.com/Calvinnix/simple-git/releases/download/v0.5.0/simple-git-linux-arm64.tar.gz"
+      sha256 "b5a409b451501325ad5b3abbed1173cca3deacca2bfb17f5db5ccba39b17331f"
     end
     on_intel do
-      url "https://github.com/Calvinnix/simple-git/releases/download/v0.4.0/simple-git-linux-amd64.tar.gz"
-      sha256 "0af3817a8190fc2eab43814ad1320e54b8511e0b0ad3b9128a8ecbe7e87589c2"
+      url "https://github.com/Calvinnix/simple-git/releases/download/v0.5.0/simple-git-linux-amd64.tar.gz"
+      sha256 "0610d3c067de98b2e9c16a3e00c2bba8b04bd870a0f77e30423c6772c89c7bdc"
     end
   end
 
