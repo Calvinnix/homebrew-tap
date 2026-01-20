a82487bafb8a47e0187c0038a977500704d4efda
Calvin Nix <Calvinnix@users.noreply.github.com>
2026-01-19 20:20:01 -0500

    Update go-on-git to v0.8.0

--- Formula/go-on-git.rb
+++ Formula/go-on-git.rb
@@ -1,28 +1,28 @@
 class GoOnGit < Formula
   desc "Lightweight Git TUI"
   homepage "https://github.com/Calvinnix/go-on-git"
-  version "0.7.0"
+  version "0.8.0"
   license "MIT"
 
   on_macos do
     on_arm do
-      url "https://github.com/Calvinnix/go-on-git/releases/download/v0.7.0/go-on-git-darwin-arm64.tar.gz"
-      sha256 "66f25149ece1e4b3c853f826a324ece3bf58dd18de2415d56fb108151eee7593"
+      url "https://github.com/Calvinnix/go-on-git/releases/download/v0.8.0/go-on-git-darwin-arm64.tar.gz"
+      sha256 "bdcd28de333956efec08fbac08fc1783ef6b5b14f58a104ea7df3afca1c5d37a"
     end
     on_intel do
-      url "https://github.com/Calvinnix/go-on-git/releases/download/v0.7.0/go-on-git-darwin-amd64.tar.gz"
-      sha256 "6ae70b41019f11cfbb6a32aab91ffc8b41c3553e2eed33cba323a44afa92be9b"
+      url "https://github.com/Calvinnix/go-on-git/releases/download/v0.8.0/go-on-git-darwin-amd64.tar.gz"
+      sha256 "3db3b05c1939ed2c2de40be988959273da7b1cc0f69fb537bc724b8d354ee6ec"
     end
   end
 
   on_linux do
     on_arm do
-      url "https://github.com/Calvinnix/go-on-git/releases/download/v0.7.0/go-on-git-linux-arm64.tar.gz"
-      sha256 "8f8034a617fd78df85e4237b0cd6a213f6a4909252e44eec66387cef6bc69564"
+      url "https://github.com/Calvinnix/go-on-git/releases/download/v0.8.0/go-on-git-linux-arm64.tar.gz"
+      sha256 "4c87720a1cbc5c0e4db1da6b7f36a8d5bc4bb6c627d65dc773c5cf4f54d7703b"
     end
     on_intel do
-      url "https://github.com/Calvinnix/go-on-git/releases/download/v0.7.0/go-on-git-linux-amd64.tar.gz"
-      sha256 "9d66b773952109db5be24d0e606257fffdf5af54f1cc299627aa2a8ccbf9893d"
+      url "https://github.com/Calvinnix/go-on-git/releases/download/v0.8.0/go-on-git-linux-amd64.tar.gz"
+      sha256 "250768d5656d7dfc3d46d8cb37b1aea1a8063596fd586ddd9cd1125a1dcf12a5"
     end
   end
 
